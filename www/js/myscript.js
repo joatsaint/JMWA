c23891b6dfd04ebf4e590b7fffd310cdbc9b56a9
joatsaint <joatsaint@hotmail.com>
2014-04-02 09:31:44 -0500

    a

--- www/js/myscript.js
+++ www/js/myscript.js
@@ -41,4 +41,31 @@ function playVideo(id, title, description) {
 	$('#myplayer').html(output);
 }
 
+players = new Array();
 
+function onYouTubeIframeAPIReady() {
+    var temp = $("iframe.yt_players");
+    for (var i = 0; i < temp.length; i++) {
+        var t = new YT.Player($(temp[i]).attr('id'), {
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+        players.push(t);
+    }
+
+}
+onYouTubeIframeAPIReady();
+
+
+function onPlayerStateChange(event) {
+
+    if (event.data == YT.PlayerState.PLAYING) {
+        var temp = event.target.a.src;
+        var tempPlayers = $("iframe.yt_players");
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].a.src != temp) players[i].stopVideo();
+
+        }
+    }
+}
